36fa1f6915a72f291dbb5ad6bfac9c8a627e5b4b
kaitinderr <ktinder2003@gmail.com>
2024-02-25 22:11:47 -0800

    main page component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,29 +1,26 @@
 import React, { useState } from 'react';
-import _ from 'lodash';
 
 function SongCard(props) {
-    const [totalScore, setTotalScore] = useState(props.song.totalScore);
-    const [numRankings, setNumRankings] = useState(props.song.numRankings);
 
     return (
         <div className="d-flex col-md-6 col-xl-3">
             <div className="card mb-2 border-0">
                 <div className="card-body ranking-card">
                     <div className="col-sm">
-                        <h3 className="card-title mb-2"><a href="wonderland_song_page.html"
+                        <h3 className="card-title mb-2"><a href="#"
                             className="text-decoration-none text-reset">{props.song.songName}</a></h3>
                         <div className="row">
                             <div className="col-sm-6">
-                                <img src={props.song.albumCover} className="card-img album-cover mb-3" alt={props.song.songName}/>
-                                    <select className="mb-3">
-                                        <option value="">Select Score</option>
-                                        <option value="5">5</option>
-                                        <option value="4">4</option>
-                                        <option value="3">3</option>
-                                        <option value="2">2</option>
-                                        <option value="1">1</option>
-                                    </select>
-                                    <button type="submit" className="">Submit</button>
+                                <img src={props.song.albumCover} className="card-img album-cover mb-3" alt={props.song.songName} />
+                                <select className="mb-3">
+                                    <option value="">Select Score</option>
+                                    <option value="5">5</option>
+                                    <option value="4">4</option>
+                                    <option value="3">3</option>
+                                    <option value="2">2</option>
+                                    <option value="1">1</option>
+                                </select>
+                                <button type="submit" className="">Submit</button>
                             </div>
                             <div className="col-sm-6">
                                 <div className="ranking-card-content">
@@ -32,13 +29,13 @@ function SongCard(props) {
                                     <p className="mb-1 category">Artist(s):</p>
                                     <p className="mb-1">{props.song.artist}</p>
                                     <p className="mb-1 category">Release Date:</p>
-                                    <p>{releaseDate}</p>
+                                    <p>{props.song.releaseDate}</p>
                                 </div>
                             </div>
                         </div>
-                        <span><strong>Rank:</strong> 1 </span>
-                        <span><strong>Score:</strong> {(totalScore/numRankings).toFixed(2)} </span>
-                        <span><strong>Ratings:</strong> {numRankings} </span>
+                        <span><strong>Rank:</strong> {props.position} </span>
+                        <span><strong>Score:</strong> {(props.song.totalScore / props.song.numRankings).toFixed(2)} </span>
+                        <span><strong>Ratings:</strong> {props.song.numRankings} </span>
                     </div>
                 </div>
             </div>
@@ -46,6 +43,27 @@ function SongCard(props) {
     )
 }
 
-export default SongList(props) {
-    let sortedSongs = _.sortBy(props.songs, props.songs)
+export default function SongList(props) {
+    const sortedSongs = props.songs.map(song => ({
+        ...song,
+        score: song.totalScore / song.numRankings
+    })).sort((a, b) => b.score - a.score);
+
+    let counter = 0;
+    const sortedSongCards = sortedSongs.map(song => {
+        counter++;
+        return <SongCard song={song} key={song.songName} position={counter}/>
+    })
+
+    return (
+        <main class="mx-auto text-center">
+            <h1>K-Pop Song Rankings</h1>
+            <input type="text" placeholder="Search..." class="search" />
+            <div class="container">
+                <div class="row">
+                    {sortedSongCards}
+                </div>
+            </div>
+        </main>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
-import SONG_DATA from './data/songs.json';
+import './index.css';
 
+import SONG_DATA from './data/songs.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
