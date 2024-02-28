7aea302317047df5f6e1955c2265a98284b89c45
cam-bun03 <wbun@uw.edu>
2024-02-27 21:58:34 -0800

    Updated labels

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -57,13 +57,14 @@ function Recommendation() {
                 <h4>Enter your recommendation:</h4>
                 <form onSubmit={handleSubmit} className="form">
                     { }
-                    <label for="songSelect">Select a Song:</label>
+                    <label for="select_song">Select a Song:
                     <select className="select_song" value={selectedSongName} onChange={handleSongChange}>
                         <option value=""></option>
                         {song.map(song => (
                             <option key={song.songName} value={song.songName}>{song.songName}</option>
                         ))}
                     </select>
+                    </label>
                     {errorMessage && <div className="error-message">{errorMessage}</div>}
                     <textarea
                         className="recommendation_text_area"
