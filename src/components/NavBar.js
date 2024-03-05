104f528de9e6057ed34ba2a433a2447f0fcc3efd
cam-bun03 <wbun@uw.edu>
2024-03-04 19:36:01 -0800

    Changed NavLink to Link for requirements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
         <header>
         <nav className="nav-bar">
-        <NavLink to="/" class="icon"><img src="img/mango-icon.png" alt="mango icon"/></NavLink>
+        <Link to="/" class="icon"><img src="img/mango-icon.png" alt="mango icon"/></Link>
             <div className="hamburger-menu"><a href="#"><i className="fa fa-bars" aria-label="menu"></i></a></div>
             <div className="nav-links">
                 <ul className="nav-options">
-                    <li className="nav-page"><NavLink to="/">Home</NavLink></li>
-                    <li className="nav-page"><NavLink to="GamePage">Game</NavLink></li>
+                    <li className="nav-page"><Link to="/">Home</Link></li>
+                    <li className="nav-page"><Link to="GamePage">Game</Link></li>
                     <li className="nav-page"><a href="#">Sign in</a></li>
                 </ul>
             </div>
