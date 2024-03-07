c561a01900b3e0f796f2003aeae13516f87ce1fd
kaitinderr <ktinder2003@gmail.com>
2024-03-07 14:02:12 -0800

    added some firebase stuff

--- src/index.js
+++ src/index.js
@@ -6,6 +6,25 @@ import './index.css';
 
 import SONG_DATA from './data/songs.json';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0udIGu6B2Nth1M9GwnFiUE0nZjVkqIh8",
+  authDomain: "mangomusic-64e1e.firebaseapp.com",
+  databaseURL: "https://mangomusic-64e1e-default-rtdb.firebaseio.com",
+  projectId: "mangomusic-64e1e",
+  storageBucket: "mangomusic-64e1e.appspot.com",
+  messagingSenderId: "218225259976",
+  appId: "1:218225259976:web:b903ff024be3cf4f0d7cfd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
