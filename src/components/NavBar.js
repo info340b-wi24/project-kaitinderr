cbaa8e61615be8bc1f7d0f507900464415ddb8bd
Byung wook Park <b2park2017@gmail.com>
2024-03-09 15:58:14 -0800

    hamburger menue addition and css changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
 
 export default function NavBar() {
-    return (
-        <header>
-        <nav className="nav-bar">
-        <Link to="/" className="icon"><img src="img/mango-icon.png" alt="mango icon"/></Link>
-            <div className="hamburger-menu"><a href="#"><i className="fa fa-bars" aria-label="menu"></i></a></div>
-            <div className="nav-links">
-                <ul className="nav-options">
-                    <li className="nav-page"><Link to="/">Home</Link></li>
-                    <li className="nav-page"><Link to="game">Game</Link></li>
-                    <li className="nav-page"><Link to="signin">Sign in</Link></li>
-                </ul>
-            </div>
-        </nav>
+  return (
+    <header>
+      <nav className="nav-bar">
+        <Link to="/" className="icon"><img src="img/mango-icon.png" alt="mango icon" /></Link>
+        
+        {/* Dropdown for mobile view */}
+        <div className="mobile-nav">
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              <i className="fa fa-bars"></i> {/* Ensure you have font-awesome or similar for the icon */}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item as="button">
+                <Link to="/">Home</Link>
+              </Dropdown.Item>
+              <Dropdown.Item as="button">
+                <Link to="/game">Game</Link>
+              </Dropdown.Item>
+              <Dropdown.Item as="button">
+                <Link to="/signin">Sign In</Link>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        
+        {/* Regular navigation for desktop view */}
+        <div className="desktop-nav">
+          <Link to="/">Home</Link>
+          <Link to="/game">Game</Link>
+          <Link to="/signin">Sign In</Link>
+        </div>
+      </nav>
     </header>
-    )
+  );
 }
