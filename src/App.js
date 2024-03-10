193d4d86f1f491936a1b7cecf42ecc5682b2fd37
kaitinderr <ktinder2003@gmail.com>
2024-03-09 21:34:01 -0800

    added router back

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Routes, Route, useNavigate, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import KpopGame from './components/KpopGame.js';
 import SongList from './components/SongList.js';
 import SongPage from './components/SongPage.js';
